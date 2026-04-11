423c57e60775b379d08aa31e01b1d92dce8449c6
Ali Hadi <rajanihadi17@gmail.com>
2026-04-11 14:28:44 +0500

    fix: Add missing CompanyUser model

---
+++ app/Models/CompanyUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyUser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'user_id',
+        'role',
+        'status',
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
